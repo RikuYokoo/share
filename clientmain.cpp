98ddaca9110fe98899f34b057d3ee2bdce98d928
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2021-09-13 17:56:30 +0900

    クライアントから送ったやつをサーバが送り返す

--- clientmain.cpp
+++ clientmain.cpp
@@ -7,11 +7,34 @@ int main(int argc, char const *argv[])
 
     
     for(int i = 0;i < 5;++i)
-        client.send(std::string("ieeeeeeeeeeeeei"));
+        client.send(std::string("RikuYokoo"));
     std::string end("end");
     // client.send(std::move(end));
     std::cout << "now end status ->"<< end << "running\n";
 
-    std::cout << "\n\nend !!!!!!!!!!!!!!!!!!!!!!!!!!!";
+    std::cout << "\n\nend !!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+
+
+//-----------------------------送ったやつを受け取る-----------
+    boost::asio::io_service io_service;
+    std::vector<std::string> stringContainer; 
+   // std::this_thread::sleep_for(1s);
+
+    InfoShareServer server(7111,[&stringContainer](std::string&& s){
+        std::string moved_string(std::move(s));
+        std::cout << moved_string;
+        stringContainer.push_back(moved_string);
+    });
+
+    std::this_thread::sleep_for(7s);
+    std::cout << "\nreturn server!!!!!!!!!!!!!\n\n";
+//    std::this_thread::sleep_for(5s);
+    server.terminate();
+    std::cout << "terminated ";
+    std::this_thread::sleep_for(3s);
+    std::cout << "-------------output-------------" << std::endl;
+    std::cout << "\n\n" << std::boolalpha << stringContainer.empty() << std::endl;
+    for(const auto& itr:stringContainer)
+        std::cout << itr << std::endl;
     return 0;
 }
--- servermain.cpp
+++ servermain.cpp
@@ -16,5 +16,21 @@ int main(int argc, char const *argv[])
     std::this_thread::sleep_for(3s);
     for(const auto& itr:stringContainer)
         std::cout << itr << std::endl;
+
+
+    //-----------送る
+    std::cout << "wait 1s" << std::endl;
+    std::this_thread::sleep_for(1s);
+    std::cout << "\nend wait 1s\n" << std::endl;
+    Client client("127.0.0.1",7111);
+
+    
+    for(int i = 0;i < stringContainer.size();++i)
+        client.send(std::move(stringContainer.at(i)));
+    std::string end("return end");
+    // client.send(std::move(end));
+    std::cout << "now return end status -> "<< end << " running\n";
+
+    std::cout << "\n\nend !!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
     return 0;
 }
