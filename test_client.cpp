7f5810b33754076e2ff02cc25c409c264a340f07
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2021-09-19 15:41:04 +0900

    Add serialization and time measurement

--- test_client.cpp
+++ test_client.cpp
@@ -1,12 +1,16 @@
 #include "header.hpp"
+#include "proto/send_data.pb.h"
 #include <vector>
 #include <string>
 using namespace  Citbrains::infosharemodule;
+using namespace InfoShare;
 
 class TestClient{
     Client client;
-    std::string send_string_data_, receive_string_data_;
+    SendData send_data, return_send_data_;//protocolbuf
+    std::string send_string_data_, receive_string_data_, serialize_data_;
     int send_times_, return_string_times_;
+    int64_t now_time_;
 public:
     TestClient(std::string address, int32_t port) : client(address, port)
     {
@@ -16,7 +20,7 @@ public:
     {
         for(int i=0;i<send_times_;++i)
         {
-            std::string tmp_send_string_data = send_string_data_;
+            std::string tmp_send_string_data = serialize_data_;
             client.send(std::move(tmp_send_string_data));
         }
             //client.send(std::move(send_string_data_));
@@ -40,13 +44,15 @@ public:
             return_string_times_ = stringContainer.size();
         }
 
+        return_send_data_.ParseFromString(receive_string_data_);
+
         return isCorrectSend();
     }
 
     bool isCorrectSend()
     {
-        bool times = (return_string_times_ == send_times_);
-        bool string = (send_string_data_ == receive_string_data_);
+        bool times = (return_send_data_.now_time() == now_time_);
+        bool string = (send_string_data_ == return_send_data_.send());
         if(times && string) return true;
         else return false;
     }
@@ -54,11 +60,15 @@ public:
     void runServer(int count, std::string tmp_send_string, int return_port)
     {
         send_string_data_ = tmp_send_string;
+        serialize_data_ = tmp_send_string;
         send_times_ = count;
 
         std::cout << "send string data = " << send_string_data_ << std::endl;
+        std::cout << "before serialize_data = " << serialize_data_ << std::endl;
         std::cout << "send times = " << send_times_ << std::endl;
-        
+        setSerializeData();
+        serializeSendData();
+        //std::cout << "after serialize_data = " << serialize_data_ << std::endl;
         sendData();
         bool is_correct = receiveReturnData(return_port);
 
@@ -75,16 +85,38 @@ public:
             std::cout << "Not data" << std::endl;
         }
     }
+
+    void serializeSendData()//TODO:エラー処理
+    {
+        send_data.SerializeToString(&serialize_data_);
+    }
+
+    void setSerializeData()
+    {
+        send_data.set_now_time(getTime());
+        send_data.set_send(send_string_data_);
+    }
+
+    int64_t getTime()
+    {
+        now_time_ = static_cast<int64_t> (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
+        std::cout << "now_time = " << now_time_ << std::endl;
+        return now_time_;
+    }
 };
 
 
 int main()
 {
     TestClient test_client("127.0.0.1", 7110);
+    //TestClient t3("127.0.0.1", 7110);
 
-    test_client.runServer(5, "YokooRiku", 7111);
+    test_client.runServer(5, "1YokooRiku", 7111);
+    //t3.runServer(2, "testdata", 7113);
 
     test_client.receiveDataPrint();
+    //std::cout << "t3 ------------\n";
+    //t3.receiveDataPrint();
 
     return 0;
 }
--- test_server.cpp
+++ test_server.cpp
@@ -1,13 +1,17 @@
 #include "header.hpp"
+#include "proto/send_data.pb.h"
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace  Citbrains::infosharemodule;
-
+using namespace InfoShare;
 class TestServer{
     //Server server;
-    std::string return_ip_address_;
+    SendData send_data;
+    std::string return_ip_address_, serial_string_;
     int32_t return_port_;
     std::vector<std::string> stringContainer; 
+    int64_t recv_time_, now_time_;
 
     void sendReceiveData()
     {
@@ -26,15 +30,21 @@ public:
     {
         boost::asio::io_service io_service;
         std::vector<std::string> tmp_receiv_string_data;
-        Server server(port, [&tmp_receiv_string_data](std::string&& s){
+        std::vector<int64_t> tmp_now_time;
+        Server server(port, [&tmp_receiv_string_data, &tmp_now_time](std::string&& s){
             std::string moved_string(std::move(s));
             std::cout << moved_string;
+            tmp_now_time.push_back(static_cast<int64_t> (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count()));
             tmp_receiv_string_data.push_back(moved_string);
         });
         std::cout << "server clieted\n";
         std::this_thread::sleep_for(5s);
 
         stringContainer = tmp_receiv_string_data;
+        serial_string_ = tmp_receiv_string_data.at(0);
+        //recv_time_ = *min_element(tmp_now_time.begin(), tmp_now_time.end());
+        recv_time_ = *max_element(tmp_now_time.begin(), tmp_now_time.end());
+
     }
 
     void returnReceiveData(std::string address, int32_t port)
@@ -43,6 +53,28 @@ public:
         return_port_ = port;
 
         sendReceiveData();
+    }
+
+    void praseSerialdata()
+    {
+        send_data.ParseFromString(serial_string_);
+    }
+
+    void isCheck()
+    {
+        std::cout << "ischeck now_time = " << send_data.now_time() << std::endl;
+        std::cout << "ischeck send_data = " << send_data.send() << std::endl;
+    }
+
+    int64_t getTime()
+    {
+        now_time_ = static_cast<int64_t> (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
+        return now_time_;
+    }
+
+    int64_t calcComunicateTime()
+    {
+        return recv_time_ - send_data.now_time();
     }
 
     /*
@@ -63,12 +95,21 @@ public:
 int main()
 {
     TestServer test_server(7110);
+    //TestServer t3(7112);
 
     //std::this_thread::sleep_for(3s);
 
     test_server.returnReceiveData("127.0.0.1", 7111);
+    //t3.returnReceiveData("127.0.0.1", 7113);
+    std::cout << "aaa-----------------------end------------------------------" << std::endl;
 
-    std::cout << "-----------------------end------------------------------" << std::endl;
+    test_server.praseSerialdata();
+    test_server.isCheck();
+
+    std::cout << "通信時間 : " << test_server.calcComunicateTime() << "[ms]" << std::endl;
+    
+    
+    std::cout << "-------------end--------------" << std::endl;
 
     return 0;
 
